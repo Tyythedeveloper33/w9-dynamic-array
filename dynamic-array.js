75e929d39c5b0affb22ce0a4f70d01780e3637d1
AllenCook <albcook1313@hotmail.com>
2024-05-11 22:12:34 -0700

    finish repo

--- dynamic-array.js
+++ dynamic-array.js
@@ -3,45 +3,77 @@ class DynamicArray {
   constructor(defaultSize = 4) {
     this.capacity = defaultSize
     this.length = 0
-    this.data = new Array(defaultSize)
+    this.data = Array(this.capacity)
   }
 
   read(index) {
     if(this.data[index] < 0) return undefined
     return this.data[index]
   }
-
+  
   push(val) {
-    this.data[data.length - 1] = val
-    this.length = this.data.length
+    if(this.length === this.capacity) {
+      this.resize()
+    }
+    
+    for(let i = 0; i < this.capacity; i++) {
+      
+      if(!this.data[i]) {
+        this.data[i] = val
+        this.length++
+        break
+      }
+    }
   }
 
-
   pop() {
-
-    // Your code here 
+    let popped;
+    for(let i = this.data.length - 1; i >= 0; i--) {
+      if(this.data[i]) {
+        popped = this.data[i]
+        this.length--
+        this.data[i] = undefined
+        break
+      }
+    }
+    return popped
   }
 
   shift() {
-
-    // Your code here 
+    let shifted = this.data[0]
+    for(let i = 0; i <= this.data.length - 1; i++) {
+        this.data[i] = this.data[i + 1]
+    }
+    this.length--
+    if(this.data[0] === undefined) this.length = 0
+    return shifted
   }
 
   unshift(val) {
-
-    // Your code here 
+    if(this.length === this.capacity) {
+      this.resize()
+    }
+    for(let i = this.data.length - 1; i >= 0; i--) {
+      if(i === 0) {
+        this.length++
+        this.data[0] = val
+        return this.data
+      }
+      this.data[i] = this.data[i - 1]
+    }
   }
 
   indexOf(val) {
-
-    // Your code here 
+    for(let i = 0; i < this.data.length; i++) {
+      if(this.data[i] === val) return i
+    }
+    return -1
   }
 
   resize() {
-
-    // Your code here 
+      this.capacity *= 2
+      this.data.length *= 2
   }
-
 }
 
 
